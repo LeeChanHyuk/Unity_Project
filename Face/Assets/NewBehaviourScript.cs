88cc75e47616878ecfead660f7c433489b9888d9
LeeChanHyuk <ne_mu@naver.com>
2020-01-14 19:00:41 +0900

    i'm so glad.. i success

---
+++ Face/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Intel.RealSense;
+using OpenCvSharp;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Pipeline pipe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pipe = new Pipeline();
+        pipe.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        using (var frames = pipe.WaitForFrames())
+        using (var depth = frames.DepthFrame)
+        {
+            print("The camera is pointing at an object " +
+                depth.GetDistance(depth.Width / 2, depth.Height / 2) + " meters away\t");
+            Mat image = new Mat(360, 640, MatType.CV_8UC3, frames.ColorFrame.Data);
+            Cv2.CvtColor(image, image, ColorConversionCodes.BGR2RGB);
+            Cv2.ImShow("image", image);
+
+        }
+    }
+}
